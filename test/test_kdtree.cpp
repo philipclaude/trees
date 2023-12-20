ef3d37b551d578555a908f4149b8cacbf5ea08f0
Philip Caplan <caplan.philip@gmail.com>
2023-12-20 17:27:56 -0500

    fix compiler warnings (#4)
    * fix sign, reorder and pragma warnings
    
    * unset compiler
    
    * add workflow
    
    * fix error writing json, run partial tests in workflow
    
    * do not show plots
    
    * reduce test sizes
    
    * fix bug in analyze script
    
    * move figures to repository root
    
    * fix image location
    
    * upload figures to pages
    
    * fix workflow syntax
    
    * specify images for artifacts
    
    * fix path
    
    * rename to trees
    
    * remove annotations
    
    * include results images in readme
    
    * rename to trees

--- test/test_kdtree.cpp
+++ test/test_kdtree.cpp
@@ -6,12 +6,11 @@
 
 struct json {
   json() { str = "{\n"; }
-  template <typename T>
-  std::string convert(const T& v) {
+  template <typename T> std::string convert(const T& v) {
     return std::to_string(v);
   }
-  template <typename T>
-  void add(const std::string& key, const T& v) {
+
+  template <typename T> void add(const std::string& key, const T& v) {
     if (n_keys > 0) str += ",\n";
     str += "\t\"" + key + "\": " + convert(v);
     n_keys++;
@@ -22,9 +21,8 @@ struct json {
   std::string str;
 };
 
-template <>
-std::string json::convert(const std::string& v) {
-  return v;
+template <> std::string json::convert(const std::string& v) {
+  return "\"" + v + "\"";
 }
 
 using index_t = uint32_t;
@@ -35,8 +33,8 @@ int main(int argc, char** argv) {
   const int dim = 3;
   Timer timer;
   bool use_zcurve = true;
-  maple::NearestNeighborApproach approach =
-      maple::NearestNeighborApproach::kRecursive;
+  trees::NearestNeighborApproach approach =
+      trees::NearestNeighborApproach::kRecursive;
   int n_neighbors = 10;
   int leaf_size = 12;
   int n_test = 1;
@@ -48,8 +46,8 @@ int main(int argc, char** argv) {
   if (argc > 4) leaf_size = std::atoi(argv[4]);
   if (argc > 5)
     approach = std::atoi(argv[5]) > 0
-                   ? maple::NearestNeighborApproach::kIterative
-                   : maple::NearestNeighborApproach::kRecursive;
+                   ? trees::NearestNeighborApproach::kIterative
+                   : trees::NearestNeighborApproach::kRecursive;
   if (argc > 6) n_test = std::atoi(argv[6]);
 
   double t_zcurve = 0;
@@ -69,16 +67,16 @@ int main(int argc, char** argv) {
     std::vector<index_t> order(n);
     timer.stop();
     std::cout << "alloc time = " << timer.seconds() << " s. " << std::endl;
-    for (auto& x : coord)
-      x = coord_t(rand()) / coord_t(RAND_MAX);
+    for (auto& x : coord) x = coord_t(rand()) / coord_t(RAND_MAX);
 
-    timer.start();
-    sort_points_on_zcurve(coord.data(), n, dim, order);
-    timer.stop();
-    std::cout << "zcurve time = " << timer.seconds() << " s. " << std::endl;
-    t_zcurve += timer.seconds() / n_test;
+    if (use_zcurve) {
+      timer.start();
+      sort_points_on_zcurve(coord.data(), n, dim, order);
+      timer.stop();
+      std::cout << "zcurve time = " << timer.seconds() << " s. " << std::endl;
+      t_zcurve += timer.seconds() / n_test;
+    }
 
-    coord_t x[dim];
     std::parafor_i(0, n, [&](int thread_id, index_t i) {
       for (int d = 0; d < dim; d++)
         points[dim * i + d] = coord[dim * order[i] + d];
@@ -89,21 +87,21 @@ int main(int argc, char** argv) {
 
     std::cout << "building kdtree for " << n / 1e6 << "M points" << std::endl;
     timer.start();
-    maple::KdTreeOptions options;
+    trees::KdTreeOptions options;
     options.leaf_size = leaf_size;
     options.parallel = true;
 
-    std::shared_ptr<maple::KdTreeNd<coord_t, index_t>> tree{nullptr};
+    std::shared_ptr<trees::KdTreeNd<coord_t, index_t>> tree{nullptr};
     if (method == 0)
-      tree = std::make_shared<maple::KdTree<dim, coord_t, index_t>>(
+      tree = std::make_shared<trees::KdTree<dim, coord_t, index_t>>(
           points.data(), n, options);
     else if (method == 1)
       tree = std::make_shared<
-          maple::KdTree_LeftBalanced<dim, coord_t, index_t, true>>(
+          trees::KdTree_LeftBalanced<dim, coord_t, index_t, true>>(
           points.data(), n, options);
     else if (method == 2) {
       tree = std::make_shared<
-          maple::KdTree_LeftBalanced<dim, coord_t, index_t, false>>(
+          trees::KdTree_LeftBalanced<dim, coord_t, index_t, false>>(
           points.data(), n, options);
     } else if (method == 3) {
       tree = std::make_shared<KdTree_nanoflann<dim, coord_t, index_t>>(
@@ -125,13 +123,13 @@ int main(int argc, char** argv) {
     std::vector<int> n_k(n_thread, 0), n_p(n_thread);
     std::parafor_i(
         0, n,
-        [&](int tid, int i) {
+        [&](int tid, size_t i) {
           int capacity = n_neighbors;
           index_t* neighbors = (index_t*)alloca(capacity * sizeof(index_t));
           coord_t* distances = (coord_t*)alloca(capacity * sizeof(coord_t));
-          using Search_t = maple::NearestNeighborSearch<index_t, coord_t>;
+          using Search_t = trees::NearestNeighborSearch<index_t, coord_t>;
           if (method == 3)
-            approach = maple::NearestNeighborApproach::kRecursive;
+            approach = trees::NearestNeighborApproach::kRecursive;
           Search_t search(n_neighbors, neighbors, distances);
           search.approach = approach;
 
@@ -161,7 +159,7 @@ int main(int argc, char** argv) {
   stats.add("t_build", t_build);
   stats.add("k", n_neighbors);
   stats.add("approach",
-            approach == maple::NearestNeighborApproach::kRecursive ? 0 : 1);
+            approach == trees::NearestNeighborApproach::kRecursive ? 0 : 1);
   stats.add("t_knn", t_knn);
   stats.add("leaf_size", used_leaf_size);
   stats.add("memory", memory);
--- util.h
+++ util.h
@@ -1,5 +1,5 @@
 //
-// mapletrees
+// trees
 // Copyright 2023 Philip Claude Caplan
 //
 // Licensed under the Apache License,
@@ -40,42 +40,34 @@ constexpr auto encoder64_2d = MortonNDLutEncoder<2, 32, 10>();
 constexpr auto encoder64_3d = MortonNDLutEncoder<3, 21, 10>();
 constexpr auto encoder64_4d = MortonNDLutEncoder<4, 16, 10>();
 
-template <int dim>
-struct Resolution;
+template <int dim> struct Resolution;
 
-template <>
-struct Resolution<2> {
+template <> struct Resolution<2> {
   // 32 bits for each coordinate
   static constexpr double value = 4294967296.0;
 };
 
-template <>
-struct Resolution<3> {
+template <> struct Resolution<3> {
   // 21 bits for each coordinate
   static constexpr double value = 2091752.0;
 };
 
-template <>
-struct Resolution<4> {
+template <> struct Resolution<4> {
   // 16 bits for each coordinate
   static constexpr double value = 65536.0;
 };
 
-template <int dim>
-morton_t encode(const std::array<uint64_t, dim>& x);
+template <int dim> morton_t encode(const std::array<uint64_t, dim>& x);
 
-template <>
-morton_t encode<2>(const std::array<uint64_t, 2>& x) {
+template <> morton_t encode<2>(const std::array<uint64_t, 2>& x) {
   return encoder64_2d.Encode(x[0], x[1]);
 }
 
-template <>
-morton_t encode<3>(const std::array<uint64_t, 3>& x) {
+template <> morton_t encode<3>(const std::array<uint64_t, 3>& x) {
   return encoder64_3d.Encode(x[0], x[1], x[2]);
 }
 
-template <>
-morton_t encode<4>(const std::array<uint64_t, 4>& x) {
+template <> morton_t encode<4>(const std::array<uint64_t, 4>& x) {
   return encoder64_4d.Encode(x[0], x[1], x[2], x[3]);
 }
 
@@ -104,11 +96,8 @@ morton_t encode_morton(const T* values, int dim, const T* xmin, const T* xmax) {
 }
 
 template <typename coord_t>
-void get_bounding_box(const coord_t* points,
-                      int64_t n_points,
-                      int8_t dim,
-                      coord_t* xmin,
-                      coord_t* xmax) {
+void get_bounding_box(const coord_t* points, int64_t n_points, int8_t dim,
+                      coord_t* xmin, coord_t* xmax) {
   std::fill(xmin, xmin + dim, std::numeric_limits<coord_t>::max());
   std::fill(xmax, xmax + dim, std::numeric_limits<coord_t>::min());
   for (int64_t i = 0; i < n_points; i++) {
@@ -121,9 +110,7 @@ void get_bounding_box(const coord_t* points,
 }
 
 template <typename coord_t, typename index_t>
-void sort_points_on_zcurve(const coord_t* points,
-                           uint64_t n_points,
-                           int8_t dim,
+void sort_points_on_zcurve(const coord_t* points, uint64_t n_points, int8_t dim,
                            std::vector<index_t>& order) {
   std::vector<coord_t> xmin(dim), xmax(dim);
   get_bounding_box(points, n_points, dim, xmin.data(), xmax.data());
@@ -134,6 +121,5 @@ void sort_points_on_zcurve(const coord_t* points,
   std::parasort(z.begin(), z.end(), [](const auto& p, const auto& q) {
     return p.second < q.second;
   });
-  for (uint64_t k = 0; k < n_points; k++)
-    order[k] = z[k].first;
+  for (uint64_t k = 0; k < n_points; k++) order[k] = z[k].first;
 }
